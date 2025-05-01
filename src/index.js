9d8b56d551dd0caf154cd6e378d9b0a3ef66a1f7
Kondwani Muwowo <168133978+kondwanimuwowo@users.noreply.github.com>
2025-05-01 11:22:26 +0300

    Add eslint

--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@ import './style.css';
 
 // Your app's JS logic here
 console.log('Todo app initialized!');
+
+
+class Todo {
+    constructor (
+
+    )
+}
+
+const toDo = [
+    {
+        projectTitle: "Project Title",
+        cretedDate: new Date(),
+        tasks: [
+            {
+                name: "Task Name",
+                description: "Descrition",
+                dueDate: new Date(),
+                status: false,
+                priority: /[1-3]/,
+            }
+        ]
+    }
+]
